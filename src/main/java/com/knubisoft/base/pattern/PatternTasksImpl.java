4796044eb8910284375ad2bf0a08f1c2267a77a5
connor999jobs <connor999jobs@gmail.com>
2022-07-28 15:00:10 +0300

    countPrivateMethodsInClass(Class<?> cls) ++ findAllFieldsForClass(Class<?> cls) ++ Queue 4+ Tree 4+ TreeNode done Pattern Done

--- src/main/java/com/knubisoft/base/pattern/PatternTasksImpl.java
+++ src/main/java/com/knubisoft/base/pattern/PatternTasksImpl.java
@@ -1,64 +1,88 @@
 package com.knubisoft.base.pattern;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PatternTasksImpl implements PatternTasks {
 
     @Override
     public boolean haveSetOfCharacters(String text) {
-        return false;
+        return text.matches("^[\\w]+$");
     }
 
     @Override
     public String matchByCharacters(String text) {
-        return null;
+        if (text.matches("pq*?"))
+            return "Found a match!";
+        else
+            return "Not matched!";
     }
 
     @Override
     public String matchStartAndEnd(String text) {
-        return null;
+        Pattern pattern = Pattern.compile("\\Bg\\B");
+        Matcher m = pattern.matcher(text);
+
+        if (m.find())
+            return "Found a match!";
+        else
+            return "Not matched!";
     }
 
     @Override
     public String matchIpAddress(String text) {
-        return null;
+        return text.replaceAll("(?<=^|\\.)0+(?!\\.|$)","");
     }
 
     @Override
     public String matchVowels(String text) {
-        return null;
+        String result = text.replaceAll("(?i)[aeiou]", "");
+        return result;
     }
 
     @Override
     public boolean validatePIN(String text) {
-        return false;
+        return text.matches("\\d{4}|\\d{6}|\\d{8}");
     }
 
     @Override
     public String divideDigit(int digit) {
-        return null;
+        String num = Integer.toString(digit);
+        int len = num.length();
+        if(len < 4) {
+            return num;
+        }
+
+        return divideDigit(Integer.parseInt(num.substring(0, len-3))) + '#' + num.substring(len-3);
     }
 
     @Override
     public String removeAllNonAlphanumericCharacters(String text) {
-        return null;
+        return text.replaceAll("(?i)[^A-Z]", "");
+
     }
 
     @Override
     public boolean validatePhoneNumber(String text) {
-        return false;
+
+        return text.matches("\\d{10}|(?:\\d{3}-){2}\\d{4}|\\(\\d{3}\\)\\d{3}-?\\d{4}");
     }
 
     @Override
     public String getLastVowelsByConstraint(String text, int n) {
-        return null;
+        String s = text.replaceAll("(?i)[^aeiou]", "");
+        int l = s.length();
+        return (n > s.length()) ? "Number of vowels mismatch!!!" : s.substring(s.length() - n);
     }
 
     @Override
     public boolean isMathematicalExpression(String text) {
-        return false;
+        return text.matches("((?:(?:^|[-+_*/])(?:\\s*-?\\d+(\\.\\d+)?(?:[eE][+-]?\\d+)?\\s*))+$)");
+
     }
 
     @Override
     public String insertDash(String text) {
-        return null;
+        return text.replaceAll("(?<=[A-Z])(?=[a-z])", "-");
     }
 }
